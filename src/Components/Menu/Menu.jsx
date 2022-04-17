ae138b62db72cb9eb9707f375ac7a54bac492ade
Abdushukur <abdulshakurturdimatov@gmail.com>
2022-04-17 17:49:43 +0500

    Some chenges in menu

--- src/Components/Menu/Menu.jsx
+++ src/Components/Menu/Menu.jsx
@@ -1,4 +1,4 @@
-import { useMutation, useQuery } from "@apollo/client";
+import { useQuery } from "@apollo/client";
 import { useParams } from "react-router-dom";
 import { MENUS } from "../../Query";
 import { useContext } from "react";
